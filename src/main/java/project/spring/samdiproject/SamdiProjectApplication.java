8a8bd1009a5946ccb45458783bbf686faa885cb2
hatnimsongi <andyfryhaha@gmail.com>
2024-10-22 22:51:11 +0900

    first

---
+++ src/main/java/project/spring/samdiproject/SamdiProjectApplication.java
@@ -0,0 +1,13 @@
+package project.spring.samdiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamdiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamdiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/spring/samdiproject/SamdiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.spring.samdiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamdiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
